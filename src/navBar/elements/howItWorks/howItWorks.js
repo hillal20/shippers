18fdef69fa5b6db538f492b4ae9ca3d0b8dd724b
hilal aissani <hilalaissani@gmail.com>
2018-12-03 19:39:01 -0500

    how ist works

--- src/navBar/elements/howItWorks/howItWorks.js
+++ src/navBar/elements/howItWorks/howItWorks.js
@@ -10,16 +10,19 @@ class HowItWorks extends Component {
   render() {
     return (
       <div className="howItWorks">
-        <div>
-          <h1>SHIPPERS, MEET CARRIERS.</h1>
-          <h1> CARRIERS, MEET SHIPPERS.</h1>
+        <div className="howItWorks-text">
+          <div>
+            <h1>SHIPPERS, MEET CARRIERS.</h1>
+            <h1> CARRIERS, MEET SHIPPERS.</h1>
+          </div>
+          <div>
+            <h1>
+              We make shipping big stuff cheap and easy by helping customers
+              directly connect with providers who have extra truck space.
+            </h1>
+          </div>
         </div>
-        <div>
-          <h1>
-            We make shipping big stuff cheap and easy by helping customers
-            directly connect with providers who have extra truck space.
-          </h1>
-        </div>
+
         <div>
           <img src={ShippersImg} />
         </div>
